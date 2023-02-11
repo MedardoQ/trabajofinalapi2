81cfa2dd0fb604ccdf59b1095da72ddcbe2c8fc4
MedardoQ <83930353+MedardoQ@users.noreply.github.com>
2023-02-11 00:58:33 -0500

    primer comentario

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'abmfirestore-39a6d',
+    appId: '1:1089385677334:web:6192dbc0fe090a2db4ab37',
+    storageBucket: 'abmfirestore-39a6d.appspot.com',
+    apiKey: 'AIzaSyBx7JENPHuRoLu60gjiZZWRMS0UL6ex7r0',
+    authDomain: 'abmfirestore-39a6d.firebaseapp.com',
+    messagingSenderId: '1089385677334',
+  },
+  production: true
+};
